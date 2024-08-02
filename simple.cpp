74fabc7dddacbc4f951f2178117eb89c86aff25d
loganrios <lrios@hey.com>
2024-08-02 15:33:00 -0600

    ocr a sample image, leptonica header nonsense

--- simple.cpp
+++ simple.cpp
@@ -1,19 +1,27 @@
 #include <tesseract/baseapi.h>
+#include <allheaders.h> // in leptonica src
 
 int main() {
   char *outText;
 
   tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
-  
-  // Initialize tesseract-ocr with English, without specifying tessdata path
-  // if (api->Init(NULL, "eng")) {
-  //   fprintf(stderr, "Could not initialize tesseract.\n");
-  //   exit(1);
-  // }
 
-  // api->End();
-  // delete api;
-  // delete[] outText;
+  // Initialize tesseract-ocr with English, without specifying tessdata path
+  if (api->Init(NULL, "eng")) {
+    fprintf(stderr, "Could not initialize tesseract.\n");
+    exit(1);
+  }
+
+  Pix *image = pixRead("../sample.png");
+  api->SetImage(image);
+
+  outText = api->GetUTF8Text();
+  printf("OCR output:\n%s", outText);
+
+  api->End();
+  delete api;
+  delete[] outText;
+  pixDestroy(&image);
 
   return 0;
 }
